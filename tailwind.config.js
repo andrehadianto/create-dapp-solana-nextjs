b476d933c19514a51a8146bf2052ae5f951e631d
Andre Hadianto <40631483+andrehadianto@users.noreply.github.com>
2021-11-03 18:07:14 +0800

    feat: update tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,39 +1,13 @@
-// default settings can be found here
-// https://unpkg.com/browse/tailwindcss@2.2.17/stubs/defaultConfig.stub.js
-
 module.exports = {
-  mode: 'jit',
-  purge: [
-    './src/**/*.{js,jsx,ts,tsx}',
-  ],
+  mode: "jit",
+  purge: ["./src/**/*.{js,jsx,ts,tsx}"],
   darkMode: "media", // or 'false' or 'class'
-  theme: {
-    fontFamily: {
-      // sans: ['Graphik', 'sans-serif'],
-      // serif: ['Merriweather', 'serif'],
-    },
-    extend: {
-      // spacing: {
-      //   '128': '32rem',
-      //   '144': '36rem',
-      // },
-      // borderRadius: {
-      //   '4xl': '2rem',
-      // }
-    },
-  },
-  variants: {
-    extend: {},
-  },
-  plugins: [
-    require('@tailwindcss/typography'),
-    require("daisyui")
-  ],
+  plugins: [require("@tailwindcss/typography"), require("daisyui")],
   daisyui: {
     styled: true,
     themes: [
       // first one will be the default theme
-      "dark",
+      // "dark",
       // uncomment to enable
       // "light (default)",
       // "dark",
@@ -43,7 +17,7 @@ module.exports = {
       // "corporate",
       // "synthwave",
       // "retro",
-      // "cyberpunk",
+      "cyberpunk",
       // "valentine",
       // "halloween",
       // "garden",
